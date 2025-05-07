80feb7ebeb3dcfc29c01619cf7a2487879205bb9
yelly <kk3p1q1@gmail.com>
2025-05-07 19:09:47 +0900

    맥북 볼륨 조절에 이어폰 예외 추가 (+디버깅코드)

--- scripts/wifi-volume-control.sh
+++ scripts/wifi-volume-control.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 TARGET_SSID="Bunjang_Office_10F"
+EXCLUDED_AUDIO_DEVICE="외장 헤드폰"
+
 CURRENT_SSID=$(networksetup -getairportnetwork en0 | cut -d ':' -f2 | xargs)
+# CURRENT_OUTPUT=$(SwitchAudioSource -c)
+CURRENT_OUTPUT=$(/opt/homebrew/bin/SwitchAudioSource -c)
+# echo "Current Output Device: $CURRENT_OUTPUT"
 
 if [ "$CURRENT_SSID" == "$TARGET_SSID" ]; then
-  osascript -e "set volume output volume 0"
+  if [[ "$CURRENT_OUTPUT" != *"$EXCLUDED_AUDIO_DEVICE"* ]]; then
+    osascript -e "set volume output volume 0"
+  fi
 fi
